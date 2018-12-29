dc99d7174304f3e68bc5aeb7f64be581ac129cb0
Andrew Pillar <me@andrewpillar.com>
2018-12-29 11:47:08 +0000

    Move Discard Collector to collector/ directory
    Collector implementations should belong together.

---
+++ collector/discard.go
@@ -0,0 +1,18 @@
+package collector
+
+import (
+	"io"
+	"io/ioutil"
+
+	"github.com/andrewpillar/thrall/errors"
+)
+
+var Discard = &collector{}
+
+type collector struct {}
+
+func (c *collector) Collect(name string, r io.Reader) error {
+	_, err := io.Copy(ioutil.Discard, r)
+
+	return errors.Err(err)
+}
--- runner/collector.go
+++ runner/collector.go
@@ -1,22 +1,7 @@
 package runner
 
-import (
-	"io"
-	"io/ioutil"
-
-	"github.com/andrewpillar/thrall/errors"
-)
-
-var Discard = &collector{}
+import "io"
 
 type Collector interface {
 	Collect(name string, r io.Reader) error
-}
-
-type collector struct {}
-
-func (c *collector) Collect(name string, r io.Reader) error {
-	_, err := io.Copy(ioutil.Discard, r)
-
-	return errors.Err(err)
 }

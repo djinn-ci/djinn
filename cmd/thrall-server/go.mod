9b1481409a00050269ea7395ba1b6f7e8c45322b
Andrew Pillar <me@andrewpillar.com>
2019-02-11 21:48:59 +0000

    Implement concurrent HTTP/HTTPS listeners

--- cmd/thrall-server/go.mod
+++ cmd/thrall-server/go.mod
@@ -2,4 +2,8 @@ module github.com/andrewpillar/thrall/cmd/thrall-server
 
 replace github.com/andrewpillar/thrall => ../../
 
-require github.com/andrewpillar/cli v1.0.13
+require (
+	github.com/andrewpillar/cli v1.0.13
+	github.com/andrewpillar/thrall v0.0.0
+	gopkg.in/yaml.v2 v2.2.2 // indirect
+)
--- cmd/thrall-server/main.go
+++ cmd/thrall-server/main.go
@@ -1,14 +1,87 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/andrewpillar/cli"
+
+	"github.com/andrewpillar/thrall/config"
 )
 
-func mainCommand(c cli.Command) {
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Thrall CI server\n"))
+}
 
+func mainCommand(c cli.Command) {
+	f, err := os.Open(c.Flags.GetString("config"))
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s: %s\n", os.Args[0], err)
+		os.Exit(1)
+	}
+
+	server, err := config.DecodeServer(f)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s: %s\n", os.Args[0], err)
+		os.Exit(1)
+	}
+
+	servers := make([]*http.Server, 0)
+
+	httpServer := &http.Server{
+		Addr:         server.Listen,
+		WriteTimeout: time.Second * 15,
+		ReadTimeout:  time.Second * 15,
+		IdleTimeout:  time.Second * 60,
+		Handler:      http.HandlerFunc(handler),
+	}
+
+	servers = append(servers, httpServer)
+
+	if server.SSL.Cert != "" && server.SSL.Key != "" {
+		httpsServer := &http.Server{
+			Addr:         server.SSL.Listen,
+			WriteTimeout: time.Second * 15,
+			ReadTimeout:  time.Second * 15,
+			IdleTimeout:  time.Second * 60,
+			Handler:      http.HandlerFunc(handler),
+		}
+
+		servers = append(servers, httpsServer)
+
+		go func() {
+			if err := httpsServer.ListenAndServeTLS(server.SSL.Cert, server.SSL.Key); err != nil {
+				fmt.Fprintf(os.Stderr, "%s\n", err)
+			}
+		}()
+	}
+
+	go func() {
+		if err := httpServer.ListenAndServe(); err != nil {
+			fmt.Fprintf(os.Stderr, "%s\n", err)
+		}
+	}()
+
+	sig := make(chan os.Signal, 1)
+
+	signal.Notify(sig, os.Interrupt)
+
+	<-sig
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(time.Second * 15))
+	defer cancel()
+
+	for _, s := range servers {
+		s.Shutdown(ctx)
+	}
+
+	fmt.Printf("shutting down\n")
 }
 
 func main() {

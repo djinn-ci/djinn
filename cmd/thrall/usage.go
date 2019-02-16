8dbecf5fbf4ea935aec17ac2c226c101308362df
Andrew Pillar <me@andrewpillar.com>
2019-02-16 18:11:23 +0000

    Fix usage typo

--- cmd/thrall/usage.go
+++ cmd/thrall/usage.go
@@ -10,7 +10,7 @@ Options:
 
   -a, --artifacts <dir>  The directory to store build artifacts
   -m, --manifest <file>  The manifest YAML file to use
-  -o, --objects <dir>    The directory to look in for object placment
+  -o, --objects <dir>    The directory to look in for object placement
   -s, --stage <stage>    The stage to run
 
   --help  Display this usage message`

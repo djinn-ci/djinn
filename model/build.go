015289e17ef3b94553cf482d7f792a4d96ed3a89
Andrew Pillar <me@andrewpillar.com>
2019-03-03 20:42:22 +0000

    Add build model relations

--- model/build.go
+++ model/build.go
@@ -10,15 +10,19 @@ import (
 )
 
 type Build struct {
-	ID          int64         `db:"id"`
-	UserID      int64         `db:"user_id"`
-	NamespaceID sql.NullInt64 `db:"namespace_id"`
-	Manifest    string        `db:"manifest"`
-	Status      Status        `db:"status"`
-	Output      string        `db:"output"`
-	CreatedAt   *time.Time    `db:"created_at"`
-	StartedAt   *pq.NullTime  `db:"started_at"`
-	FinishedAt  *pq.NullTime  `db:"finished_at"`
+	ID          int64          `db:"id"`
+	UserID      int64          `db:"user_id"`
+	NamespaceID sql.NullInt64  `db:"namespace_id"`
+	Manifest    string         `db:"manifest"`
+	Status      Status         `db:"status"`
+	Output      sql.NullString `db:"output"`
+	CreatedAt   *time.Time     `db:"created_at"`
+	StartedAt   *pq.NullTime   `db:"started_at"`
+	FinishedAt  *pq.NullTime   `db:"finished_at"`
+
+	User      *User
+	Namespace *Namespace
+	Tags      []*BuildTag
 }
 
 type BuildTag struct {

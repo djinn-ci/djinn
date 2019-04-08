33bcc2334e37ea1ba9a6310869021351b16d3077
Andrew Pillar <me@andrewpillar.com>
2019-04-08 09:22:07 +0100

    Set did_fail default to false

--- revisions/1554662767/up.sql
+++ revisions/1554662767/up.sql
@@ -3,7 +3,7 @@ CREATE TABLE stages (
 	build_id    INT NOT NULL REFERENCES builds(id),
 	name        VARCHAR(32) NOT NULL,
 	can_fail    BOOLEAN NOT NULL,
-	did_fail    BOOLEAN NOT NULL,
+	did_fail    BOOLEAN NOT NULL DEFAULT false,
 	status      status DEFAULT 'queued',
 	started_at  TIMESTAMP NULL,
 	finished_at TIMESTAMP NULL

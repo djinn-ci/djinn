c0f40b82de7b19d6f52df3e53378355dfae4b0cb
Andrew Pillar <me@andrewpillar.com>
2019-05-05 22:24:52 +0100

    Remove did_fail column

--- revisions/1554662767/up.sql
+++ revisions/1554662767/up.sql
@@ -3,7 +3,6 @@ CREATE TABLE stages (
 	build_id    INT NOT NULL REFERENCES builds(id),
 	name        VARCHAR(32) NOT NULL,
 	can_fail    BOOLEAN NOT NULL,
-	did_fail    BOOLEAN NOT NULL DEFAULT false,
 	status      status DEFAULT 'queued',
 	created_at  TIMESTAMP NOT NULL DEFAULT NOW(),
 	updated_at  TIMESTAMP NOT NULL DEFAULT NOW(),

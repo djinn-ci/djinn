0c11b4bc7d14d4abe00d30bd9641aec3e9441d2e
Andrew Pillar <me@andrewpillar.com>
2019-05-26 18:56:35 +0100

    Don't make artifact hash NULL

--- revisions/1556178102/up.sql
+++ revisions/1556178102/up.sql
@@ -2,7 +2,7 @@ CREATE TABLE artifacts (
 	id         SERIAL PRIMARY KEY,
 	build_id   INT NOT NULL REFERENCES builds(id) ON DELETE CASCADE,
 	job_id     INT NOT NULL REFERENCES jobs(id),
-	hash       VARCHAR NULL UNIQUE,
+	hash       VARCHAR UNIQUE,
 	source     VARCHAR NOT NULL,
 	name       VARCHAR NOT NULL,
 	size       INT NULL,

3c8b4eb86d34a7815dd67be34e662bb6e9fdb7b0
Andrew Pillar <me@andrewpillar.com>
2019-06-20 22:45:20 +0100

    Add HashNow for hashing the current UNIX nano

--- crypto/crypto.go
+++ crypto/crypto.go
@@ -1,6 +1,8 @@
 package crypto
 
 import (
+	"time"
+
 	"github.com/andrewpillar/thrall/errors"
 
 	"github.com/speps/go-hashids"
@@ -32,4 +34,20 @@ func Hash(i []int) (string, error) {
 	id, err := hd.Encode(i)
 
 	return id, errors.Err(err)
+}
+
+// Create a hash of the current UNIX nano timestamp.
+func HashNow() (string, error) {
+	i := make([]int, 0)
+
+	now := time.Now().UnixNano()
+
+	for now != 0 {
+		i = append(i, int(now % 10))
+		now /= 10
+	}
+
+	h, err := Hash(i)
+
+	return h, errors.Err(err)
 }

12c8bfcad7ae362cb2f440e1345878565c846a1f
Andrew Pillar <me@andrewpillar.com>
2019-08-24 13:40:04 +0100

    Add logic for collaborator creation

--- web/ui/collaborator.go
+++ web/ui/collaborator.go
@@ -2,16 +2,65 @@ package ui
 
 import (
 	"net/http"
+	"strconv"
 
+	"github.com/andrewpillar/thrall/errors"
+	"github.com/andrewpillar/thrall/log"
+	"github.com/andrewpillar/thrall/model"
+	"github.com/andrewpillar/thrall/template"
 	"github.com/andrewpillar/thrall/web"
+
+	"github.com/gorilla/mux"
 )
 
 type Collaborator struct {
 	web.Handler
+
+	Invites model.InviteStore
 }
 
 func (h Collaborator) Store(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
 
+	id, _ := strconv.ParseInt(vars["invite"], 10, 64)
+
+	i, err := h.Invites.Find(id)
+
+	if err != nil {
+		log.Error.Println(errors.Err(err))
+		h.FlashAlert(w, r, template.Danger("Failed to accept invite: " + errors.Cause(err).Error()))
+		http.Redirect(w, r, r.Header.Get("Referer"), http.StatusSeeOther)
+		return
+	}
+
+	if err := i.LoadNamespace(); err != nil {
+		log.Error.Println(errors.Err(err))
+		h.FlashAlert(w, r, template.Danger("Failed to accept invite: " + errors.Cause(err).Error()))
+		http.Redirect(w, r, r.Header.Get("Referer"), http.StatusSeeOther)
+		return
+	}
+
+	collaborators := i.Namespace.CollaboratorStore()
+
+	c := collaborators.New()
+	c.UserID = i.InviteeID
+
+	if err := collaborators.Create(c); err != nil {
+		log.Error.Println(errors.Err(err))
+		h.FlashAlert(w, r, template.Danger("Failed to accept invite: " + errors.Cause(err).Error()))
+		http.Redirect(w, r, r.Header.Get("Referer"), http.StatusSeeOther)
+		return
+	}
+
+	if err := h.Invites.Delete(i); err != nil {
+		log.Error.Println(errors.Err(err))
+		h.FlashAlert(w, r, template.Danger("Failed to accept invite: " + errors.Cause(err).Error()))
+		http.Redirect(w, r, r.Header.Get("Referer"), http.StatusSeeOther)
+		return
+	}
+
+	h.FlashAlert(w, r, template.Success("You are now a collaborator in: " + i.Namespace.Name))
+	http.Redirect(w, r, r.Header.Get("Referer"), http.StatusSeeOther)
 }
 
 func (h Collaborator) Destroy(w http.ResponseWriter, r *http.Request) {

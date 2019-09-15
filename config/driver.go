352efcad355c8c2626b70fc76051a939ac625c7f
Andrew Pillar <me@andrewpillar.com>
2019-09-15 17:40:18 +0100

    Add driver config

---
+++ config/driver.go
@@ -0,0 +1,39 @@
+package config
+
+import (
+	"io"
+
+	"github.com/andrewpillar/thrall/errors"
+
+	"github.com/pelletier/go-toml"
+)
+
+type Driver struct {
+	Config map[string]string `toml:"-"`
+
+	SSH  SSH
+	Qemu Qemu
+}
+
+type SSH struct {
+	User    string
+	Timeout int
+}
+
+type Qemu struct {
+	Disks  string
+	Port   int64
+	CPUs   int
+	Memory int
+}
+
+func DecodeDriver(r io.Reader) (Driver, error) {
+	dec := toml.NewDecoder(r)
+	driver := Driver{}
+
+	if err := dec.Decode(&driver); err != nil {
+		return driver, errors.Err(err)
+	}
+
+	return driver, nil
+}

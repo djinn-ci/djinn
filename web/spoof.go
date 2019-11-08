af75c83d5d074df8d1ff2a872c9cee1b67b126a9
Andrew Pillar <me@andrewpillar.com>
2019-11-08 22:09:47 +0000

    Shallow copy the request during spoofing to get _method field

--- web/spoof.go
+++ web/spoof.go
@@ -21,13 +21,18 @@ func (h Spoof) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		typ := r.Header.Get("Content-Type")
 
 		if strings.Contains(typ, "application/x-www-form-urlencoded") || strings.Contains(typ, "multipart/form-data") {
-			if err := r.ParseForm(); err != nil {
+			// Shallow copy HTTP request so the original request body does not
+			// get parsed. We don't want to accidentally discard
+			// multipart/form-data requests, this can cause CSRF to fail.
+			tmp := r.WithContext(r.Context())
+
+			if err := tmp.ParseForm(); err != nil {
 				log.Error.Println(errors.Err(err))
 				h.Handler.ServeHTTP(w, r)
 				return
 			}
 
-			method := r.Form.Get("_method")
+			method := tmp.Form.Get("_method")
 
 			if method == "PATCH" || method == "PUT" || method == "DELETE" {
 				r.Method = method

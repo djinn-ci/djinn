e35ef57a91af91ef38c2f2ef6e820fc5b2a1d2ae
Andrew Pillar <me@andrewpillar.com>
2020-09-26 16:44:44 +0100

    Add comments to cron batcher

--- cron/batcher.go
+++ cron/batcher.go
@@ -7,6 +7,8 @@ import (
 	"github.com/andrewpillar/query"
 )
 
+// Batcher provides a way of retrieving batches of cron jobs that are ready
+// to be executed.
 type Batcher struct {
 	err       error
 	paginator database.Paginator
@@ -14,6 +16,8 @@ type Batcher struct {
 	crons     []*Cron
 }
 
+// NewBatcher returns a new Batcher using the given Store to retrieve cron jobs
+// from, and setting the size of each batch to the given limit.
 func NewBatcher(s *Store, limit int64) *Batcher {
 	return &Batcher{
 		store:     s,
@@ -24,6 +28,10 @@ func NewBatcher(s *Store, limit int64) *Batcher {
 	}
 }
 
+// Next will load in the next batch of cron jobs to be executed
+// (WHERE NOW() >= next_run). This will return false if it reaches the end of
+// the batches in the table. If the end of the table is reached, or if an error
+// happens then false is returned.
 func (b *Batcher) Next() bool {
 	paginator, err := b.store.Paginate(b.paginator.Page, query.WhereRaw("NOW()", ">=", "next_run"))
 
@@ -53,6 +61,8 @@ func (b *Batcher) Next() bool {
 	return paginator.Page == paginator.Pages[len(paginator.Pages)-1]
 }
 
+// Crons returns the slice of crons from the current batch.
 func (b *Batcher) Crons() []*Cron { return b.crons }
 
+// Err returns the current error, if any, that occurred when loading a batch.
 func (b *Batcher) Err() error { return b.err }

224968796834bba1f39e9aea0c4165f6d92a0301
Andrew Pillar <me@andrewpillar.com>
2020-11-05 19:18:06 +0000

    Package comments

--- version/version.go
+++ version/version.go
@@ -1,6 +1,9 @@
+// Package version contains version information about the built Go binaries.
+// The variables exported by this package will be set during the build time
+// via the -ldflags with -X.
 package version
 
 var (
-	Ref string
-	Tag string
+	Ref string // Ref is the output of "git rev-parse HEAD"
+	Tag string // Tag is the current tag of the git repository
 )

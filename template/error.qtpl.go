468ccabb1c5a4a680c696f3cde6cbfd99ea58cd1
Andrew Pillar <me@andrewpillar.com>
2021-04-03 18:11:13 +0100

    Render encoded stacktrace in a readonly textarea

--- template/error.qtpl.go
+++ template/error.qtpl.go
@@ -69,7 +69,7 @@ func (p *Error) StreamFooter(qw422016 *qt422016.Writer) {
 //line template/error.qtpl:23
 	qw422016.N().S(` <style type="text/css">`)
 //line template/error.qtpl:24
-	qw422016.N().S(`*{margin:0;padding:0}a{color:#66c9ff;cursor:pointer;text-decoration:none}body{font-family:sans-serif;font-size:14px;background:#383e51;color:#fff}h1,h2{font-weight:400}.error{margin:0 auto;margin-top:250px;max-width:400px;padding:20px;text-align:center}.error .logo{margin:0 auto;margin-bottom:20px;width:0}.error .logo .handle{margin-left:-20px;border-style:solid;border-width:5px 0 12px 10px;border-color:transparent transparent transparent #fff}.error .logo .lid{margin-bottom:-30px;margin-left:5px;border-style:solid;border-width:5px 0 12px 10px;border-color:transparent transparent transparent #fff}.error .logo .lantern{margin-left:-25px;border-style:solid;border-width:25px 25px 75px 0;border-color:transparent #fff transparent transparent}.error h2{margin-top:20px}pre{text-align:left}`)
+	qw422016.N().S(`*{margin:0;padding:0}a{color:#66c9ff;cursor:pointer;text-decoration:none}body{font-family:sans-serif;font-size:14px;background:#383e51;color:#fff}h1,h2{font-weight:400}.error{margin:0 auto;margin-top:250px;max-width:400px;padding:20px;text-align:center}.error .logo{margin:0 auto;margin-bottom:20px;width:0}.error .logo .handle{margin-left:-20px;border-style:solid;border-width:5px 0 12px 10px;border-color:transparent transparent transparent #fff}.error .logo .lid{margin-bottom:-30px;margin-left:5px;border-style:solid;border-width:5px 0 12px 10px;border-color:transparent transparent transparent #fff}.error .logo .lantern{margin-left:-25px;border-style:solid;border-width:25px 25px 75px 0;border-color:transparent #fff transparent transparent}.error h2{margin-top:20px}textarea{font-family:monospace;text-align:center;box-sizing:border-box;min-width:100%;max-width:100%;min-height:1625px;max-height:1625px;border:solid 1px rgba(255,255,255,.3);border-radius:3px;background:rgba(0,0,0,.3);color:#fff}textarea:focus{border:solid 1px rgba(255,255,255,.5)}`)
 //line template/error.qtpl:24
 	qw422016.N().S(`</style> `)
 //line template/error.qtpl:25
@@ -153,11 +153,11 @@ func (p *InternalError) StreamBody(qw422016 *qt422016.Writer) {
 //line template/error.qtpl:49
 	qw422016.E().S(p.Message)
 //line template/error.qtpl:49
-	qw422016.N().S(`</h2> <br/> <a href="/">Back</a> <br/><br/> <pre>`)
+	qw422016.N().S(`</h2> <br/> <a href="/">Back</a> <br/><br/> <textarea readonly>`)
 //line template/error.qtpl:53
 	qw422016.E().S(p.Stack)
 //line template/error.qtpl:53
-	qw422016.N().S(`</pre> </div> `)
+	qw422016.N().S(`</textarea> </div> `)
 //line template/error.qtpl:55
 }
 

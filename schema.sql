c12667b5f583e85b47bb1e3d00fc81cc859ab16c
Andrew Pillar <me@andrewpillar.com>
2021-05-04 17:47:45 +0100

    Update schema with webhook tables

--- schema.sql
+++ schema.sql
@@ -1,5 +1,7 @@
 
 
+DROP TABLE IF EXISTS namespace_webhook_deliveries;
+DROP TABLE IF EXISTS namespace_webhooks;
 DROP TABLE IF EXISTS oauth_tokens;
 DROP TABLE IF EXISTS oauth_codes;
 DROP TABLE IF EXISTS oauth_apps;
@@ -320,4 +322,32 @@ CREATE TABLE oauth_tokens (
 	scope      BYTEA NOT NULL,
 	created_at TIMESTAMP NOT NULL DEFAULT NOW(),
 	updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE namespace_webhooks (
+	id           SERIAL PRIMARY KEY,
+	user_id      INT NOT NULL REFERENCES users(id),
+	author_id    INT NOT NULL REFERENCES users(id),
+	namespace_id INT NOT NULL REFERENCES namespaces(id),
+	payload_url  VARCHAR NOT NULL,
+	secret       VARCHAR NULL,
+	ssl          BOOLEAN NOT NULL,
+	events       BYTEA NOT NULL,
+	active       BOOLEAN NOT NULL DEFAULT TRUE,
+	created_at   TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE namespace_webhook_deliveries (
+	id               SERIAL,
+	webhook_id       INT NOT NULL REFERENCES namespace_webhooks(id) ON DELETE CASCADE,
+	delivery_id      VARCHAR NOT NULL,
+	redelivery       BOOLEAN NOT NULL DEFAULT FALSE,
+	request_headers  VARCHAR NOT NULL,
+	request_body     VARCHAR NOT NULL, 
+	response_code    INT NOT NULL,
+	repsonse_headers VARCHAR NOT NULL,
+	response_body    VARCHAR NULL,
+	duration         INT NOT NULL,
+	created_at       TIMESTAMP NOT NULL DEFAULT NOW(),
+	PRIMARY KEY(id, delivery_id)
 );

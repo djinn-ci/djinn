0b6870dcd7135c44340a86a76962deecac80dfac
Andrew Pillar <me@andrewpillar.com>
2021-05-18 22:46:37 +0100

    Dispatch build_tagged event

--- build/handler/tag.go
+++ build/handler/tag.go
@@ -5,11 +5,13 @@ import (
 
 	"djinn-ci.com/build"
 	"djinn-ci.com/database"
+	"djinn-ci.com/env"
 	"djinn-ci.com/errors"
 	"djinn-ci.com/namespace"
 	"djinn-ci.com/user"
 	"djinn-ci.com/web"
 
+	"github.com/andrewpillar/query"
 	"github.com/andrewpillar/webutil"
 
 	"github.com/gorilla/mux"
@@ -61,7 +63,36 @@ func (h Tag) StoreModel(r *http.Request) ([]*build.Tag, error) {
 	}
 
 	tt, err := build.NewTagStore(h.DB, b).Create(u.ID, f.Tags...)
-	return tt, errors.Err(err)
+
+	if err != nil {
+		return nil, errors.Err(err)
+	}
+
+	h.Queue.Enqueue(func() error {
+		if !b.NamespaceID.Valid {
+			return nil
+		}
+
+		n, err := namespace.NewStore(h.DB).Get(query.Where("id", "=", query.Arg(b.NamespaceID)))
+
+		if err != nil {
+			return errors.Err(err)
+		}
+
+		jsontags := make([]map[string]interface{}, 0, len(tt))
+
+		for _, t := range tt {
+			jsontags = append(jsontags, t.JSON(env.DJINN_API_SERVER))
+		}
+
+		v := map[string]interface{}{
+			"url": env.DJINN_API_SERVER + b.Endpoint("tags"),
+			"tags": jsontags,
+		}
+		return namespace.NewWebhookStore(h.DB, n).Deliver("build_tagged", v)
+	})
+
+	return tt, nil
 }
 
 func (h Tag) DeleteModel(r *http.Request) error {

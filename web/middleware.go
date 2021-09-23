fc10700e9fbed15f66fa5be209c9fc292da079ea
Andrew Pillar <me@andrewpillar.com>
2021-09-23 18:48:03 +0100

    Respond with JSON when Content-Type is for JSON

--- web/middleware.go
+++ web/middleware.go
@@ -184,7 +184,8 @@ func (h Middleware) Gate(gates ...Gate) mux.MiddlewareFunc {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			var errh errorHandler = HTMLError
 
-			json := strings.HasPrefix(r.Header.Get("Accept"), "application/json")
+			json := strings.HasPrefix(r.Header.Get("Accept"), "application/json") ||
+				strings.HasPrefix(r.Header.Get("Content-Type"), "application/json")
 
 			if json {
 				errh = JSONError

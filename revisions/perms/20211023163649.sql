ca5a33f7039e9034c49a0cebe5f04f029e659177
Andrew Pillar <me@andrewpillar.com>
2021-10-24 10:45:58 +0100

    Fix revision typo

--- revisions/perms/20211023163649.sql
+++ revisions/perms/20211023163649.sql
@@ -8,7 +8,7 @@ Create djinn_consumer user
 CREATE USER djinn_consumer;
 
 GRANT CONNECT ON DATABASE djinn TO djinn_consumer;
-GRANT SELEC ON image_downloads TO djinn_server;
+GRANT SELECT ON image_downloads TO djinn_server;
 
 GRANT SELECT ON images TO djinn_consumer;
 GRANT SELECT, UPDATE ON image_downloads TO djinn_consumer;

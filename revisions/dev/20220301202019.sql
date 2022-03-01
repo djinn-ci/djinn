5aff2d22adda1460466b92f599268b1b8a130a5b
Andrew Pillar <me@andrewpillar.com>
2022-03-01 20:21:01 +0000

    Add revision for new table columns

---
+++ revisions/dev/20220301202019.sql
@@ -0,0 +1,9 @@
+/*
+Revision: dev/20220301202019
+Author:   Andrew Pillar <me@andrewpillar.com>
+
+Add columns for variable masking
+*/
+
+ALTER TABLE variables ADD COLUMN masked BOOLEAN DEFAULT FALSE;
+ALTER TABLE build_variables ADD COLUMN masked BOOLEAN DEFAULT FALSE;
